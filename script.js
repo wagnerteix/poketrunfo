8320f8137f3fc547e0bb471c31986edc1510a8ec
Wagner Teixeira <113645593+wagnerteix@users.noreply.github.com>
2022-09-17 03:41:52 -0300

    Add files via upload
    Card-game estilo Super Trunfo com temática de Pokémon.
    Regras: só pode utilizar a carta do adversário caso tenha derrotado ela. Quem atingir 5 vitórias primeiro vence o jogo.

---
+++ script.js
@@ -0,0 +1,532 @@
+var carta1 = {
+  nome: "Venusaur",
+  atributos: {
+    HP: 80,
+    Attack: 82,
+    Defense: 83,
+    Speed: 80
+  },
+  img:
+    "https://pa1.narvii.com/6261/1f3c0dfe4bded6a247e4c6484bfd2777494881f4_hq.gif"
+};
+
+var carta2 = {
+  nome: "Blastoise",
+  atributos: {
+    HP: 79,
+    Attack: 83,
+    Defense: 100,
+    Speed: 78
+  },
+  img: "https://gifs.eco.br/wp-content/uploads/2022/04/gifs-do-blastoise-3.gif"
+};
+
+var carta3 = {
+  nome: "Charizard",
+  atributos: {
+    HP: 78,
+    Attack: 84,
+    Defense: 78,
+    Speed: 100
+  },
+  img:
+    "http://24.media.tumblr.com/a3c9d29e51c9fd099456eb2898884891/tumblr_mrcm96Dfcq1r3maj7o1_500.gif"
+};
+
+var carta4 = {
+  nome: "Meganium",
+  atributos: {
+    HP: 80,
+    Attack: 82,
+    Defense: 100,
+    Speed: 80
+  },
+  img: "https://c.tenor.com/9fC96LF3mU8AAAAC/meganium.gif"
+};
+
+var carta5 = {
+  nome: "Typhlosion",
+  atributos: {
+    HP: 78,
+    Attack: 84,
+    Defense: 78,
+    Speed: 100
+  },
+  img: "https://thumbs.gfycat.com/MaleTanGlowworm-size_restricted.gif"
+};
+
+var carta6 = {
+  nome: "Feraligatr",
+  atributos: {
+    HP: 85,
+    Attack: 105,
+    Defense: 100,
+    Speed: 78
+  },
+  img:
+    "http://pa1.narvii.com/6199/6de30a24b181b17c74e2460f3dae4163ed8868c3_00.gif"
+};
+
+var carta7 = {
+  nome: "Sceptile",
+  atributos: {
+    HP: 70,
+    Attack: 85,
+    Defense: 65,
+    Speed: 120
+  },
+  img:
+    "https://pa1.narvii.com/6717/8ce4a60cef0afa33a2dc1c4e423d6e7ab55ae056_hq.gif"
+};
+
+var carta8 = {
+  nome: "Blaziken",
+  atributos: {
+    HP: 80,
+    Attack: 120,
+    Defense: 70,
+    Speed: 80
+  },
+  img: "https://i.gifer.com/Fc7j.gif"
+};
+
+var carta9 = {
+  nome: "Swampert",
+  atributos: {
+    HP: 100,
+    Attack: 110,
+    Defense: 90,
+    Speed: 60
+  },
+  img:
+    "https://i.pinimg.com/originals/48/47/5d/48475d7584881d43fb04d4f94c96912a.gif"
+};
+
+var carta10 = {
+  nome: "Arcanine",
+  atributos: {
+    HP: 90,
+    Attack: 110,
+    Defense: 80,
+    Speed: 95
+  },
+  img: "https://c.tenor.com/ffcdLXaEV84AAAAC/arcanine-flamethrower.gif"
+};
+
+var carta11 = {
+  nome: "Scyther",
+  atributos: {
+    HP: 70,
+    Attack: 110,
+    Defense: 80,
+    Speed: 105
+  },
+  img: "https://i.imgur.com/KRU2486.gif"
+};
+
+var carta12 = {
+  nome: "Gyarados",
+  atributos: {
+    HP: 95,
+    Attack: 125,
+    Defense: 79,
+    Speed: 81
+  },
+  img:
+    "https://s-media-cache-ak0.pinimg.com/originals/59/08/5d/59085d51f3a184dfeb3ee0cfdc6f159f.gif"
+};
+
+var carta13 = {
+  nome: "Infernape",
+  atributos: {
+    HP: 76,
+    Attack: 104,
+    Defense: 71,
+    Speed: 108
+  },
+  img: "https://c.tenor.com/RTwTX_Cw3K0AAAAC/infernape-pokemon-infernape.gif"
+};
+
+var carta14 = {
+  nome: "Aggron",
+  atributos: {
+    HP: 70,
+    Attack: 110,
+    Defense: 180,
+    Speed: 50
+  },
+  img: "http://25.media.tumblr.com/tumblr_mdf0kuuR7n1qd87hlo1_500.gif"
+};
+
+var carta15 = {
+  nome: "Torterra",
+  atributos: {
+    HP: 95,
+    Attack: 109,
+    Defense: 105,
+    Speed: 56
+  },
+  img: "https://c.tenor.com/_38YOS_D794AAAAM/torterra-frenzy-plant.gif"
+};
+
+var carta16 = {
+  nome: "Empoleon",
+  atributos: {
+    HP: 84,
+    Attack: 86,
+    Defense: 88,
+    Speed: 60
+  },
+  img: "https://i.gifer.com/origin/97/97e8022b6b294681412f9a4378fded44_w200.gif"
+};
+
+var carta17 = {
+  nome: "Dragonite",
+  atributos: {
+    HP: 91,
+    Attack: 134,
+    Defense: 95,
+    Speed: 80
+  },
+  img:
+    "https://pa1.narvii.com/6212/1374831f2a6ce38d546e54c309928b58ce3f383e_hq.gif"
+};
+
+var carta18 = {
+  nome: "Wobbuffet",
+  atributos: {
+    HP: 190,
+    Attack: 33,
+    Defense: 58,
+    Speed: 33
+  },
+  img: "https://c.tenor.com/Of9ae_nb2o8AAAAC/wobbuffet-pokemon.gif"
+};
+
+var carta19 = {
+  nome: "Tyranitar",
+  atributos: {
+    HP: 100,
+    Attack: 134,
+    Defense: 110,
+    Speed: 61
+  },
+  img: "https://24.media.tumblr.com/tumblr_m8ss2iPSf11rc4lweo1_500.gif"
+};
+
+var carta20 = {
+  nome: "Mamoswine",
+  atributos: {
+    HP: 110,
+    Attack: 130,
+    Defense: 80,
+    Speed: 80
+  },
+  img: "https://c.tenor.com/zhtIKl_n3VMAAAAC/pokemon-mamoswine.gif"
+};
+
+var cartas = [
+  carta1,
+  carta2,
+  carta3,
+  carta4,
+  carta5,
+  carta6,
+  carta7,
+  carta8,
+  carta9,
+  carta10,
+  carta11,
+  carta12,
+  carta13,
+  carta14,
+  carta15,
+  carta16,
+  carta17,
+  carta18,
+  carta19,
+  carta20
+];
+var cartaMaquina;
+var cartaJogador;
+
+var cartasDoJogador = [];
+var cartasDaMaquina = [];
+var placarJogador = 0;
+var placarMaquina = 0;
+
+var rareCandy = {
+  img:
+    "https://1.bp.blogspot.com/-6cvpMv9IBRk/US1rYrAWHXI/AAAAAAAAATA/2K0jN9tm8n8/s1600/Dream_Rare_Candy_Sprite.png"
+};
+var pokeCenter = {
+  img:
+    "https://static.wikia.nocookie.net/pokefanbattleuniverse/images/2/28/Calis_Pokemon_Gym_by_UltimateTraveler.png"
+};
+var pokeball = {
+  img: "https://pwo-wiki.info/images/b/b1/Pok%C3%A9ball.png"
+};
+
+var whos = {
+  img: "https://i.ytimg.com/vi/LtVJR-jxoho/maxresdefault.jpg"
+};
+
+var insignia1 = {
+  img:
+    "https://cdn.staticneo.com/w/pokemon/thumb/7/7d/Soul_Badge.png/50px-Soul_Badge.png"
+};
+
+var insignia2 = {
+  img:
+    "https://cdn.staticneo.com/w/pokemon/thumb/d/dd/Boulder_Badge.png/50px-Boulder_Badge.png"
+};
+
+var insignia3 = {
+  img:
+    "https://cdn.staticneo.com/w/pokemon/thumb/9/9c/Cascade_Badge.png/50px-Cascade_Badge.png"
+};
+
+var insignia4 = {
+  img:
+    "https://cdn.staticneo.com/w/pokemon/thumb/a/a6/Thunder_Badge.png/50px-Thunder_Badge.png"
+};
+
+var insignia5 = {
+  img:
+    "https://cdn.staticneo.com/w/pokemon/thumb/b/b5/Rainbow_Badge.png/50px-Rainbow_Badge.png"
+};
+
+var insignia6 = {
+  img:
+    "https://cdn.staticneo.com/w/pokemon/thumb/6/6b/Marsh_Badge.png/50px-Marsh_Badge.png"
+};
+
+var insignia7 = {
+  img:
+    "https://cdn.staticneo.com/w/pokemon/thumb/1/12/Volcano_Badge.png/50px-Volcano_Badge.png"
+};
+
+var insignia8 = {
+  img:
+    "https://cdn.staticneo.com/w/pokemon/thumb/7/78/Earth_Badge.png/50px-Earth_Badge.png"
+};
+
+var insignias = [
+  insignia1,
+  insignia2,
+  insignia3,
+  insignia4,
+  insignia5,
+  insignia6,
+  insignia7,
+  insignia8
+];
+var imagens = [rareCandy, pokeball, pokeCenter, whos];
+
+function sortearCarta() {
+  var numeroCartaMaquina = parseInt(Math.random() * cartas.length);
+  cartaMaquina = cartas[numeroCartaMaquina];
+
+  while (cartasDoJogador.includes(cartaMaquina)) {
+    numeroCartaMaquina = parseInt(Math.random() * cartas.length);
+    cartaMaquina = cartas[numeroCartaMaquina];
+  }
+  cartasDaMaquina.push(cartaMaquina);
+
+  var numeroCartaJogador = parseInt(Math.random() * cartas.length);
+  while (numeroCartaMaquina == numeroCartaJogador) {
+    numeroCartaJogador = parseInt(Math.random() * cartas.length);
+  }
+  cartaJogador = cartas[numeroCartaJogador];
+
+  while (cartasDaMaquina.includes(cartaJogador)) {
+    numeroCartaJogador = parseInt(Math.random() * cartas.length);
+    cartaJogador = cartas[numeroCartaJogador];
+  }
+  cartasDoJogador.push(cartaJogador);
+
+  document.getElementById("btnSortear").disabled = true;
+  document.getElementById("btnJogar").disabled = false;
+  document.getElementById("btnReiniciar").disabled = true;
+  document.getElementById("naoEscolheu").innerHTML = "";
+
+  exibirCartaJogador();
+}
+
+function obtemAtributoSelecionado() {
+  var radioAtributos = document.getElementsByName("atributo");
+  var escolhaRadio = 0;
+
+  for (var i = 0; i < radioAtributos.length; i++) {
+    if (radioAtributos[i].checked == true) {
+      return radioAtributos[i].value;
+    } else {
+      escolhaRadio++;
+      if (escolhaRadio == radioAtributos.length) {
+        var elementoEscolha = document.getElementById("naoEscolheu");
+        elementoEscolha.innerHTML =
+          "Você não escolheu nenhuma opção, então iremos comparar HP e ver se você sobrevive!";
+        return radioAtributos[0].value;
+      }
+    }
+  }
+}
+
+function jogar() {
+  var atributoSelecionado = obtemAtributoSelecionado();
+  var divResultado = document.getElementById("resultado");
+
+  if (
+    cartaJogador.atributos[atributoSelecionado] >
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    htmlResultado =
+      `<br>O atributo comparado foi ${atributoSelecionado}.<br>Você venceu essa batalha e será recompensado com uma rare candy! <img src=` +
+      rareCandy["img"] +
+      ` style="width: 50px; height: 50px;">`;
+    cartasDoJogador.push(cartaMaquina);
+    placarJogador++;
+  } else if (
+    cartaJogador.atributos[atributoSelecionado] <
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    htmlResultado =
+      `<br>O atributo comparado foi ${atributoSelecionado}.<br>Você perdeu essa batalha, seu Pokémon precisará ir ao PokeCenter! <img src=` +
+      pokeCenter["img"] +
+      ` style="width: 80px; height: 80px;">`;
+    cartasDaMaquina.push(cartaJogador);
+    placarMaquina++;
+  } else {
+    htmlResultado =
+      `<br>O atributo comparado foi ${atributoSelecionado}.<br>Houve um empate, voltem para suas Pokeball! <img src=` +
+      pokeball["img"] +
+      ` style="width: 35px; height: 35px;">`;
+  }
+
+  if (placarMaquina == 5 || placarJogador == 5) {
+    document.getElementById("btnJogar").disabled = true;
+    document.getElementById("btnRodada").disabled = true;
+    document.getElementById("btnSortear").disabled = true;
+
+    if (placarMaquina == 5) {
+      htmlResultado =
+        `<p class='acabou'> Fim de Jogo! O vencedor foi a máquina por ${placarMaquina} a ${placarJogador}.<br>Seus Pokémon estão gravemente feridos, leve-os o mais rápido ao PokeCenter! <img src=` +
+        pokeCenter["img"] +
+        ` style="width: 80px; height: 80px;"></p>`;
+      document.getElementById("btnReiniciar").innerHTML =
+        "<button type='button' onclick='reiniciar()'>Jogar Novamente</button>";
+    } else if (placarJogador == 5) {
+      htmlResultado =
+        `<p class='acabou'>Fim de Jogo! O vencedor foi você por ${placarJogador} a ${placarMaquina}.<br>Por isso você será recompensado com uma Insignia! <img src=` +
+        insignias[parseInt(Math.random() * insignias.length)].img +
+        ` style="width: 40px; height: 40px;"></p>`;
+      document.getElementById("btnReiniciar").innerHTML =
+        "<button type='button' onclick='reiniciar()'>Jogar Novamente</button>";
+    }
+  } else {
+    document.getElementById("btnRodada").disabled = false;
+  }
+
+  divResultado.innerHTML = htmlResultado;
+
+  document.getElementById("btnJogar").disabled = true;
+  exibirCartaMaquina();
+  exibirPlacar();
+}
+
+function exibirCartaJogador() {
+  var divCartaMaquina = document.getElementById("carta-maquina");
+  divCartaMaquina.style.backgroundImage = `url(${whos.img})`;
+
+  var divCartaJogador = document.getElementById("carta-jogador");
+  divCartaJogador.style.backgroundImage = `url(${cartaJogador.img})`;
+  //divCartaJogador.style.backgroundImage = "url(" + cartaJogador.img + ")"
+  //as duas linhas são a mesma coisa, mas escritas de formas diferentes
+  var moldura =
+    '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+
+  var opcoesTexto = "";
+  for (var atributo in cartaJogador.atributos) {
+    opcoesTexto +=
+      "<br/>" +
+      "<input type='radio' name='atributo' value='" +
+      atributo +
+      "'style='margin: 0px;'>  " +
+      atributo +
+      " " +
+      cartaJogador.atributos[atributo] +
+      " <br>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaJogador.nome ?? ""}</p>`;
+
+  divCartaJogador.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+  divCartaMaquina.innerHTML = moldura + "</div>";
+}
+
+function exibirCartaMaquina() {
+  var divCartaMaquina = document.getElementById("carta-maquina");
+  divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.img})`;
+  //divCartaJogador.style.backgroundImage = "url(" + cartaJogador.img + ")"
+  //as duas linhas são a mesma coisa, mas escritas de formas diferentes
+  var moldura =
+    '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+
+  var opcoesTexto = "";
+  for (var atributo in cartaMaquina.atributos) {
+    opcoesTexto +=
+      "<br/>" +
+      "<p type='text' name='atributo' value='" +
+      atributo +
+      "'style='margin: 0px;'>" +
+      atributo +
+      " " +
+      cartaMaquina.atributos[atributo] +
+      "</p>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaMaquina.nome ?? ""}</p>`;
+
+  divCartaMaquina.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
+
+function proxRodada() {
+  document.getElementById("btnSortear").disabled = false;
+  document.getElementById("btnRodada").disabled = true;
+  document.getElementById("resultado").innerHTML = "";
+  document.getElementById("carta-jogador").innerHTML = "";
+  document.getElementById("carta-maquina").innerHTML = "";
+  document.getElementById("carta-jogador").style.backgroundImage = "";
+  document.getElementById("carta-maquina").style.backgroundImage = "";
+  document.getElementById("naoEscolheu").innerHTML = "";
+
+  sortearCarta();
+}
+
+function exibirPlacar() {
+  var pontos = document.getElementById("placar");
+  pontos.innerHTML =
+    `<span>Você: ` +
+    placarJogador +
+    `</span> | <span>Máquina: ` +
+    placarMaquina +
+    `</span>`;
+}
+
+function reiniciar() {
+  document.getElementById("btnSortear").disabled = false;
+  document.getElementById("btnRodada").disabled = true;
+  document.getElementById("resultado").innerHTML = "";
+  document.getElementById("placar").innerHTML = "";
+  document.getElementById("carta-jogador").innerHTML = "";
+  document.getElementById("carta-maquina").innerHTML = "";
+  document.getElementById("carta-jogador").style.backgroundImage = "";
+  document.getElementById("carta-maquina").style.backgroundImage = "";
+  document.getElementById("naoEscolheu").innerHTML = "";
+
+  placarMaquina = 0;
+  placarJogador = 0;
+
+  cartasDaMaquina = [];
+  cartasDoJogador = [];
+}
